48984be6411e4ac2f58779791cef0a729d7b0ccd
AmiePhyo <amie.phyo@my.jcu.edu.au>
2025-07-06 19:53:13 +0800

    commit car and used_cars files after adding str()  function

---
+++ prac_06/used_cars.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 Practical - Client code to use the Car class.
+Note that the import has a folder (module) in it.
+This is why we name our folders with no spaces or capitals, as valid module names.
+"""
+
+from prac_06.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+
+    my_car = Car(180)
+    my_car.drive(30)
+    print(f"Car has fuel: {my_car.fuel}")
+    print(my_car)
+
+    limo =  Car(100)
+    limo.add_fuel(20)
+    print(f"The amount of fuel in limo is {limo.fuel}")
+    distance_driven = limo.drive(115)
+    print(f"Limo drove {distance_driven}km")
+
+    my_car = Car(42)
+    my_car._odometer = 227
+    print(str(my_car))
+
+
+main()
