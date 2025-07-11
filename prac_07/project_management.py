3c385c125c16197c72dc247cd3b905242e1762d0
AmiePhyo <amie.phyo@my.jcu.edu.au>
2025-07-12 04:04:43 +0800

    Commit display_projects function of project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -20,11 +20,21 @@ def main():
     while choice != "q":
         if choice == "l":
             filename = input("Filename to load projects from: ")
-            load_projects(filename)
+            projects = load_projects(filename)
         elif choice == "s":
             filename = input("Filename to save projects to: ")
             save_projects(projects,filename)
+        elif choice == "d":
+            display_projects(projects)
 
+        print("- (L)oad projects")
+        print("- (S)ave projects")
+        print("- (D)isplay projects")
+        print("- (F)ilter projects by date")
+        print("- (A)dd new project")
+        print("- (U)pdate project")
+        print("- (Q)uit")
+        choice = input(">>> ").lower()
 
 def load_projects(filename=FILENAME):
     """Loads projects from a file."""
@@ -47,5 +57,18 @@ def save_projects(projects, filename=FILENAME):
             line = f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}\t{project.priority}\t{project.cost_estimate}\t{project.completion_percent}\n"
             file.write(line)
     print(f"Saved {len(projects)} projects to {filename}")
+
+def display_projects(projects):
+    """Displays projects group;ed by completion and sorted by priority."""
+    incomplete_projects = sorted([project for project in projects if not project.is_complete()])
+    complete_projects = sorted(([project for project in projects if project.is_complete()]))
+    print("Incompleted projects:")
+    for project in incomplete_projects:
+        print(f"{project}")
+
+    print("Completed projects:")
+    for project in complete_projects:
+        print(f"{project}")
+
 
 main()
