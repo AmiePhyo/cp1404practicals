071ae4ae7caa03cbd8a642fb264f3b9835fb4354
AmiePhyo <amie.phyo@my.jcu.edu.au>
2025-07-12 03:19:18 +0800

    Commit save_projects function of project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,7 +7,7 @@ FILENAME = "projects.txt"
 
 def main():
     print("Welcome to Pythonic Project Management!")
-    load_projects()
+    projects = load_projects()
     print("- (L)oad projects")
     print("- (S)ave projects")
     print("- (D)isplay projects")
@@ -21,6 +21,9 @@ def main():
         if choice == "l":
             filename = input("Filename to load projects from: ")
             load_projects(filename)
+        elif choice == "s":
+            filename = input("Filename to save projects to: ")
+            save_projects(projects,filename)
 
 
 def load_projects(filename=FILENAME):
@@ -35,5 +38,14 @@ def load_projects(filename=FILENAME):
             projects.append(Project(name, start_date, int(priority), float(cost), int(completion)))
     print(f"Loaded {len(projects)} projects from {filename}")
     return projects
+
+def save_projects(projects, filename=FILENAME):
+    """Saves projects to a file."""
+    with open(filename, "w") as file:
+        file.write("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage\n")
+        for project in projects:
+            line = f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}\t{project.priority}\t{project.cost_estimate}\t{project.completion_percent}\n"
+            file.write(line)
+    print(f"Saved {len(projects)} projects to {filename}")
 
 main()
