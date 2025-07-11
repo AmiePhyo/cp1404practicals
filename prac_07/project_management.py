f5c4cafcf39f0be1865c698563e17db3ba243a44
AmiePhyo <amie.phyo@my.jcu.edu.au>
2025-07-12 03:14:36 +0800

    Commit load_projects function of project_management.py

---
+++ prac_07/project_management.py
@@ -0,0 +1,39 @@
+import datetime
+
+from project import Project
+
+FILENAME = "projects.txt"
+
+
+def main():
+    print("Welcome to Pythonic Project Management!")
+    load_projects()
+    print("- (L)oad projects")
+    print("- (S)ave projects")
+    print("- (D)isplay projects")
+    print("- (F)ilter projects by date")
+    print("- (A)dd new project")
+    print("- (U)pdate project")
+    print("- (Q)uit")
+    choice = input(">>> ").lower()
+
+    while choice != "q":
+        if choice == "l":
+            filename = input("Filename to load projects from: ")
+            load_projects(filename)
+
+
+def load_projects(filename=FILENAME):
+    """Loads projects from a file."""
+    projects = []
+    with open(filename, "r") as file:
+        file.readline()
+        for line in file:
+            parts = line.strip().split("\t")
+            name, date_str, priority, cost, completion = parts
+            start_date = datetime.datetime.strptime(date_str, "%d/%m/%Y").date()
+            projects.append(Project(name, start_date, int(priority), float(cost), int(completion)))
+    print(f"Loaded {len(projects)} projects from {filename}")
+    return projects
+
+main()
